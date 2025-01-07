6d3eded7cb588210e1751cac0799b1f671635bd7
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2025-01-07 23:43:21 +0300

    Initialzed the communication between API and the ESP

---
+++ Connection/Connection.ino
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include "AsyncJson.h"
+#include "ArduinoJson.h"
+AsyncWebServer server(80);
+
+const char* ssid = "nashef"; 
+const char* password = "";
+
+void notFound(AsyncWebServerRequest *request)
+{
+  request->send(404, "application/json", "{\"message\":\"Not found\"}");
+}
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
+    Serial.printf("WiFi Failed!\n");
+  }
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  AsyncCallbackJsonWebHandler *handler = new AsyncCallbackJsonWebHandler("/post-message", [](AsyncWebServerRequest *request, JsonVariant &json) {
+    StaticJsonDocument<200> data;
+    if (json.is<JsonArray>())
+    {
+      data = json.as<JsonArray>();
+    }
+    else if (json.is<JsonObject>())
+    {
+      data = json.as<JsonObject>();
+    }
+    String response;
+    serializeJson(data, response);
+    request->send(200, "application/json", response);
+    Serial.println(response);
+  });
+  server.addHandler(handler);
+server.onNotFound(notFound);
+  server.begin();
+}
+void loop()
+{
+}
